465f3e9eca1726cc57d1ce461904002ad862294e
HP <HP@DESKTOP-6J3GBF9>
2024-05-08 21:23:35 +0530

    This is First Commit

---
+++ src/er/BubbleSort.java
@@ -0,0 +1,32 @@
+package er;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] array = new int[4];
+		
+		array[0] = 89;
+		array[1] = 23;
+		array[2] = 14;
+		array[3] = 45;
+		
+		for(int i=1; i<(array.length); i++) {
+			for(int j = 1; j<=(array.length - i); j++) {
+				if(array[j-1]> array[j]) {
+					int x = array[j];
+					array[j] = array[j-1];
+					array[j-1] = x;
+				}
+			}
+		}
+		
+		for(int n = 0; n<4; n++) {
+			System.out.println(array[n]);
+			
+		}
+
+	}
+
+}
